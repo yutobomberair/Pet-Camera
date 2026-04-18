d95dd893be49ea6673d5b7c50a12ab1e2f59f1a2
Yuto Onuma <yutobomberair@icloud.com>
2026-04-18 18:02:01 +0100

    tmp

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import cv2
+import time
 import numpy as np
-import tensorflow as tf
+import tflite_runtime.interpreter as tflite
+
+from motion_dtection.MotionDetection_module import MotionDetection_module 
 
 # =========================
 # モデルロード
 # =========================
-interpreter = tf.lite.Interpreter(
+interpreter = tflite.Interpreter(
     model_path="training/yolov8n-cls_float32.tflite"  # ← 必ず自分のbestモデルから変換したやつ
 )
 interpreter.allocate_tensors()
@@ -26,63 +29,108 @@ classes = ["Dog", "Person", "Cat", "Bird"]
 # =========================
 cap = cv2.VideoCapture(0)
 
+# =========================
+# config
+# =========================
+STATE = {0: "md", 1: "dnn", 2: "fr"}
+det_fps = 10
+conf_thresh = 90
+md_configs = {
+    "md_h": 30,
+    "md_v": 24,
+    "bufnum": 25, 
+    "update_period": 5,
+    "pix_thresh": 25,
+    "num_thresh": 20,
+}
+
+# =========================
+# process
+# =========================
 if not cap.isOpened():
     print("❌ カメラ開けてない")
     exit()
 
+fps = cap.get(cv2.CAP_PROP_FPS)
+print("FPS: ", fps)
+drop = fps / det_fps
+
+current_state = STATE[0]
+frm_count = 0
+md = MotionDetection_module()
 while True:
-    ret, frame = cap.read()
-    if not ret:
-        print("❌ フレーム取得失敗")
-        break
+    if current_state != "fr" and frm_count < drop:
+        frm_count += 1
+        continue
 
-    # =========================
-    # 前処理
-    # =========================
-    img = cv2.resize(frame, (320, 320))
-    img = img.astype(np.float32) / 255.0
-    img = np.expand_dims(img, axis=0)  # (1, 320, 320, 3)
+    elif current_state == "md":
+        frm_count = 0
+        ret, frame = cap.read()
+        if not ret:
+            print("❌ フレーム取得失敗")
+            break
+        md.update_buffer(frame)
+        current_state = "dnn"
+        print("a")
+        continue
 
-    # =========================
-    # 推論
-    # =========================
-    interpreter.set_tensor(input_details[0]['index'], img)
-    interpreter.invoke()
+    elif current_state == "dnn":
+        ret, frame = cap.read()
+        if not ret:
+            print("❌ フレーム取得失敗")
+            break
 
-    output = interpreter.get_tensor(output_details[0]['index'])[0]
+        # =========================
+        # 前処理
+        # =========================
+        img = cv2.resize(frame, (320, 320))
+        img = img.astype(np.float32) / 255.0
+        img = np.expand_dims(img, axis=0)  # (1, 320, 320, 3)
 
-    # =========================
-    # 後処理
-    # =========================
-    pred = int(np.argmax(output))
-    conf = float(output[pred])
+        # =========================
+        # 推論
+        # =========================
+        interpreter.set_tensor(input_details[0]['index'], img)
+        interpreter.invoke()
 
-    # 安全対策（1000クラス問題防止）
-    if pred >= len(classes):
-        label = f"Unknown({pred})"
-    else:
-        label = classes[pred]
+        output = interpreter.get_tensor(output_details[0]['index'])[0]
 
-    # =========================
-    # 描画
-    # =========================
-    text = f"{label}: {conf:.2f}"
-    cv2.putText(
-        frame,
-        text,
-        (10, 30),
-        cv2.FONT_HERSHEY_SIMPLEX,
-        1,
-        (0, 255, 0),
-        2
-    )
+        # =========================
+        # 後処理
+        # =========================
+        pred = int(np.argmax(output))
+        conf = float(output[pred])
 
-    print(text)
+        # 安全対策（1000クラス問題防止）
+        if pred < len(classes):
+            label = classes[pred]
+        else:
+            label = "Unknown"
 
-    cv2.imshow("camera", frame)
+        if label == "Dog" and conf >= conf_thresh:
+            current_state = "fr"
 
-    if cv2.waitKey(1) & 0xFF == 27:
-        break
+        # =========================
+        # 描画
+        # =========================
+        text = f"{label}: {conf:.2f}"
+        cv2.putText(
+            frame,
+            text,
+            (10, 30),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1,
+            (0, 255, 0),
+            2
+        )
+
+        # print(text)
+
+        cv2.imshow("camera", frame)
+
+        if cv2.waitKey(1) & 0xFF == 27:
+            break
+    count = 0
 
 cap.release()
 cv2.destroyAllWindows()
---
+++ motion_detection/MotionDetection_module.py
@@ -0,0 +1,48 @@
+
+class MotionDetection_module:
+    def __init__(self, config):
+        self.md_h = config.md_h
+        self.md_v = config.md_v
+        self.bufnum = config.bufnum
+        self.update_period = config.update_period
+        self.pix_thresh = config.pix_thresh
+        self.num_thresh = config.num_thresh
+        self.reset()
+
+    def update_buffer(self, img):
+        if self.buf_isfull != 1:
+            proc_img = self.preprocess(img)
+            self.buffer[self.buf_idx] = proc_img
+            self.buf_idx += 1
+            if self.buf_idx == self.bufnum:
+                self.buf_isfull = 1
+                self.buf_idx = 0
+        elif self.buf_cnt > self.update_period:
+            proc_img = self.preprocess(img)
+            self.buffer[self.buf_idx] = proc_img
+            self.buf_idx = (self.buf_idx + 1) % self.bufnum
+            self.buf_cnt = 0
+        else:
+            self.buf_cnt += 1
+
+    def preprocess(self, img):
+        gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        bin_img = cv2.resize(img, (self.md_h, self.md_v), interpolation=cv2.INTER_AREA)
+        return bin_img
+
+    def detect(self, img):
+        proc_img = self.preprocess(img)
+        ave_buffer = np.mean(self.buffer, axis=0)
+        diff = cv2.absdiff(proc_img, ave_buffer.astype(np.uint8))
+        diff_norm = diff.astype(np.float32) / 255
+        mask = (diff_norm > self.pix_thresh).astype(np.uint8)
+        if np.sum(mask) > self.num_thresh:
+            return 1
+        else:
+            return 0
+
+    def reset(self):
+        self.buf_cnt = 0
+        self.buf_idx = 0
+        self.buf_isfull = 0
+        self.buffer = np.zeros((self.bufnum, self.md_h, self.md_v, 1), dtype=np.uint8)
